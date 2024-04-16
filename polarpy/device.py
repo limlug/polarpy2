42f9a47894020da2041ac57e4620503f0254e064
Jonas Pöhler <jonas.poehler@uni-siegen.de>
2024-04-16 18:33:33 +0200

    Added Support for Bleak

--- polarpy/device.py
+++ polarpy/device.py
@@ -5,9 +5,9 @@ from .settings import StreamSettings
 from .commands import Commands
 
 from collections import deque
-from pygatttool import PyGatttool
-
-
+from bleak import BleakClient, BleakScanner
+from loguru import logger
+import asyncio
 class CallbacksImpl(Callbacks):
     def __init__(self, callback=None):
         self._callback = callback
@@ -86,11 +86,12 @@ class Device:
         self.stream_settings = StreamSettings()
         self._callbacks = CallbacksImpl(callback=callback)
 
-        self._ble = PyGatttool(address=self._addr)
-
+        #self._ble = PyGatttool(address=self._addr)
+        self._ble = None
+        self._device = None
         self._parser = Parser(
             stream_settings=self.stream_settings, callbacks=self._callbacks)
-
+        self.is_started = False
         if DeviceType.H10 == type:
             self.stream_settings.ACC_sample_rate = SampleRateSetting.SampleRate200
             self.stream_settings.ECG_sample_rate = SampleRateSetting.SampleRate200
@@ -99,37 +100,77 @@ class Device:
             self.stream_settings.ACC_sample_rate = SampleRateSetting.SampleRate50
             self.stream_settings.PPG_sample_rate = SampleRateSetting.SampleRate135
 
-    def connect(self) -> bool:
-        if not self._ble.connect():
-            return False
+    async def connect(self) -> bool:
+        scanner = BleakScanner()
+        #while self._device is None:
+        #    logger.info("Scanning....")
+        #    self._device = await scanner.find_device_by_address(self._addr)
+        logger.debug("Connecting...")
+        self._ble = BleakClient(self._addr)
+        await self._ble.connect()
+        #if not self._ble.connect():
+        #    return False
+        #svcs = await self._ble.get_services()
+        #print("Services:", svcs)
+        #for service in svcs:
+        #    logger.info(service)
+        #for service in self._ble.services:
+        #    logger.info("[Service] {0}: {1} {2}".format(service.uuid, service.description,
+        #                                                [(e.uuid, e.properties) for e in service.characteristics]))
+        #self._ble.char_write_req(handle=self._control_ccc_handle, value=0x200)
+        #self._ble.char_write_req(handle=self._data_ccc_handle, value=0x100)
+        self._control_service = self._ble.services["fb005c80-02e7-f387-1cad-8acd2d8df0c8"]
+        #def await_response(handle: int, data: bytes) -> None:
+        #    logger.debug(f"h:{handle}, data:{data}")
+        #await self._ble.start_notify("fb005c22-02e7-f387-1cad-8acd2d8df0c8", await_response)
+        #await self._ble.start_notify("fb005c26-02e7-f387-1cad-8acd2d8df0c8", await_response)
 
-        self._ble.char_write_req(handle=self._control_ccc_handle, value=0x200)
-        self._ble.char_write_req(handle=self._data_ccc_handle, value=0x100)
-        self._ble.mtu(232)
-        value = self._ble.char_read_hnd(self._control_handle)
-        self._parser.parse(value)
+
+        await self._ble.write_gatt_char(self._control_service.characteristics[1],  int(512).to_bytes(2, "big"))
+        await self._ble.write_gatt_char(self._control_service.characteristics[0], int(256).to_bytes(2, "big"))
+        #self._ble.mtu(232)
+        #value = self._ble.char_read_hnd(self._control_handle)
+        value = await self._ble.read_gatt_char("fb005c81-02e7-f387-1cad-8acd2d8df0c8")
+        #logger.debug(value)
+        #logger.debug(self._parser.parse(value))
 
         return True
 
-    def send_command(self, command: bytearray):
-        result = self._ble.char_write_cmd(self._control_handle, command)
-        return self._parser.parse(result)
+    async def send_command(self, command: bytearray):
+        #result = self._ble.char_write_cmd(self._control_handle, command)
+        event = asyncio.Event()
+        event.clear()
+        def await_response(handle: int, data: bytes) -> None:
+            #logger.debug(f"h:{handle}, data:{data}")
+            event.set()
+        await self._ble.start_notify(self._control_service.characteristics[0], await_response)
+        result = await self._ble.write_gatt_char(self._control_service.characteristics[1], command, response=True)
+        r = await self._ble.read_gatt_char(self._control_service.characteristics[0])
+        #logger.debug(await self._ble.read_gatt_char(self._control_service.characteristics[0]))
+        #logger.debug(await self._ble.read_gatt_char(self._control_service.characteristics[1]))
+        #logger.debug(result)
+        #await event.wait()
+        return self._parser.parse(r)
 
-    def start(self) -> bool:
-        if not self.connect():
+    async def start(self) -> bool:
+        if not await self.connect():
             return False
 
-        self.send_command(Commands.GetACCSettings)
-        self.send_command(Commands.GetPPGSettings)
+        await self.send_command(Commands.GetACCSettings)
+        await self.send_command(Commands.GetPPGSettings)
 
-        self.send_command(Commands.OH1StartPPG)
-        self.send_command(Commands.OH1StartACC)
-
+        await self.send_command(Commands.OH1StartPPG)
+        await self.send_command(Commands.OH1StartACC)
+        self.is_started = True
         return True
 
-    def run(self):
-        data = self._ble.wait_for_notification(handle=self._data_handle)
-        return self._parser.parse(data)
+    async def run(self):
+        def await_response(handle: int, data: bytes) -> None:
+            #logger.debug(f"h:{handle}, data:{data}")
+            self._parser.parse(data)
+        await self._ble.start_notify("fb005c82-02e7-f387-1cad-8acd2d8df0c8", await_response)
+
+        #return self._parser.parse(data)
 
 
 class OH1(Device):
